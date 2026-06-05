9ce82cc7b6808a4d33ac3e398e4a74fbe025f54a
Moises Rojas <ssj52949@gmail.com>
2026-06-05 16:43:50 -0500

    dasdsdasd

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // eslint-disable-next-line prettier/prettier
   plugins: [
-    TanStackRouterVite(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
     tsconfigPaths(),
   ],
   build: {
